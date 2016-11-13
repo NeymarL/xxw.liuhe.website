2b466e604409d4802fae5e861590df2232408f32
NeymarL <lh57506155@qq.com>
2016-11-13 15:46:57 +0800

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |        my-controller/my-method    -> my_controller/my_method
  */
 
-$route['default_controller'] = 'user';
+$route['default_controller'] = 'user/home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = false;
 
